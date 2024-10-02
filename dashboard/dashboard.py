31c340b52f92194b966bc57f62976b059a26d4c6
zfathii <fathimah.zulfah@gmail.com>
2024-10-03 00:56:35 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -42,7 +42,7 @@ with st.sidebar:
     with col1:
         st.write(' ')
     with col2:
-        st.image("https://raw.githubusercontent.com/mhdhfzz/data-analyst-dicoding/main/dashboard/logo.png", width=100)
+        st.image("https://raw.githubusercontent.com/mhdhfzz/bismillah/main/dashboard/logo.png", width=100)
     with col3:
         st.write(' ')
 
